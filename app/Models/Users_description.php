207c8849f73493be4627681a9a35569fb4da665d
Christopher Chirino <critijo@gmail.com>
2021-01-30 15:09:04 -0400

    API User - Login and Register

---
+++ app/Models/Users_description.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Users_description extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'social_security',
+        'birthdate',
+        'phone_number',
+        'image',
+        'type_of_person',
+        'user_id',
+    ];
+
+    public function users(){
+        return $this->belongsTo('App\Models\User');
+    }
+}
